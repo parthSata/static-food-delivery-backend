1d5ef7ec10e26d1fd14a4f54f031ce787a61266e
Parth <parthsata1709@gmail.com>
2024-06-10 00:09:07 +0530

    d

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 import { create, router as _router, defaults } from "json-server";
-import path from "path"; 
+import path from "path";
+import fs from "fs";
 
+// Source and destination paths
+const sourcePath = path.join("/", "db.json");
+const writablePath = path.join("/tmp", "db.json");
+
+// Copy db.json from the root directory to the /tmp directory
+fs.copyFileSync(sourcePath, writablePath);
+
+// Create the JSON server
 const server = create();
-const writablePath = path.join("/tmp", "db.json"); // Change to a writable directory
 const router = _router(writablePath);
 const middlewares = defaults();
 
+// Use default middlewares and set up routing for /api
 server.use(middlewares);
-server.use("/api", router); // All API requests will be served from /api
+server.use("/api", router);
 
-const port = process.env.PORT || 3000; // Use a different port for the JSON server
+// Set the port to an environment variable or default to 3000
+const port = process.env.PORT || 3000;
+
+// Start the JSON server
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
