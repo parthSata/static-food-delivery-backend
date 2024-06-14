f84557fa0ef2806d6baf370c2fbd8ecad8ae142f
Parth <parthsata1709@gmail.com>
2024-06-14 19:53:12 +0530

    testing

--- api/server.js
+++ api/server.js
@@ -1,30 +1,30 @@
 // See https://github.com/typicode/json-server#module
-const jsonServer = require("json-server");
+const jsonServer = require('json-server')
 
-const server = jsonServer.create();
+const server = jsonServer.create()
 
 // Uncomment to allow write operations
-const fs = require("fs");
-const path = require("path");
-const filePath = path.join("db.json");
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('db.json')
 const data = fs.readFileSync(filePath, "utf-8");
 const db = JSON.parse(data);
-const router = jsonServer.router(db);
+const router = jsonServer.router(db)
 
 // Comment out to allow write operations
 // const router = jsonServer.router('db.json')
 
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults()
 
-server.use(middlewares);
+server.use(middlewares)
 // Add this before server.use(router)
-server.use(router);
+server.use(router)  
 
 const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
-  console.log("JSON Server is running");
-});
+    console.log('JSON Server is running')
+})
 
 // Export the Server API
-module.exports = server;
+module.exports = server
