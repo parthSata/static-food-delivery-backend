2a87a18a9e98ed70a00a451284b13ff624813ff5
Parth <parthsata1709@gmail.com>
2024-06-14 19:37:03 +0530

    testing

--- api/server.js
+++ api/server.js
@@ -1,18 +1,29 @@
-const jsonServer = require("json-server");
-const path = require("path");
-const net = require("net");
+// See https://github.com/typicode/json-server#module
+const jsonServer = require('json-server')
 
-const server = jsonServer.create();
-const router = jsonServer.router(path.join("./", "db.json"));
-const middlewares = jsonServer.defaults();
+const server = jsonServer.create()
 
-server.use(middlewares);
-server.use(jsonServer.bodyParser);
-server.use(router);
+// Uncomment to allow write operations
+// const fs = require('fs')
+// const path = require('path')
+// const filePath = path.join('db.json')
+// const data = fs.readFileSync(filePath, "utf-8");
+// const db = JSON.parse(data);
+// const router = jsonServer.router(db)
 
-const port = process.env.PORT || 3000;
+// Comment out to allow write operations
+const router = jsonServer.router('db.json')
 
-server.listen(port, () => {
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+// Add this before server.use(router)
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
+server.use(router)
+server.listen(3000, () => {
     console.log('JSON Server is running')
 })
 
