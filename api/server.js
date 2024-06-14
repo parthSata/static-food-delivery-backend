a391dc79134512d032bdf39539b0b94fc19fcf46
Parth <parthsata1709@gmail.com>
2024-06-14 19:33:23 +0530

    server testing

--- api/server.js
+++ api/server.js
@@ -1,30 +1,42 @@
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
+const jsonServer = require('json-server');
+const path = require('path');
+const net = require('net');
 
-const server = jsonServer.create()
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const middlewares = jsonServer.defaults();
 
-// Uncomment to allow write operations
-const fs = require('fs')
-const path = require('path')
-const filePath = path.join('db.json')
-const data = fs.readFileSync(filePath, "utf-8");
-const db = JSON.parse(data);
-const router = jsonServer.router(db)
-
-// Comment out to allow write operations
-// const router = jsonServer.router('db.json')
-
-const middlewares = jsonServer.defaults()
-
-server.use(middlewares)
-// Add this before server.use(router)
-server.use(router)  
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.use(router);
 
 const port = process.env.PORT || 3000;
 
-server.listen(port, () => {
-    console.log('JSON Server is running')
-})
+const checkPort = (port, callback) => {
+    const tester = net.createServer()
+        .once('error', err => {
+            if (err.code === 'EADDRINUSE') {
+                callback(false);
+            } else {
+                callback(err);
+            }
+        })
+        .once('listening', () => {
+            tester.once('close', () => callback(true)).close();
+        })
+        .listen(port);
+};
 
-// Export the Server API
-module.exports = server
+checkPort(port, isFree => {
+    if (isFree === true) {
+        server.listen(port, () => {
+            console.log(`JSON Server is running on port ${port}`);
+        });
+    } else if (isFree === false) {
+        console.error(`Port ${port} is already in use. Please free the port or use another one.`);
+    } else {
+        console.error(`Error checking port: ${isFree}`);
+    }
+});
+
+module.exports = server;
