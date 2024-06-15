b6fa84b8d46cbbcf530c187bad98348b9914728a
Parth <parthsata1709@gmail.com>
2024-06-15 08:52:41 +0530

    testing

--- api/server.js
+++ api/server.js
@@ -1,30 +1,10 @@
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
+const jsonServer = require("json-server"); // importing json-server library
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000; //  chose port from here like 8080, 3001
 
-const server = jsonServer.create()
+server.use(middlewares);
+server.use(router);
 
-// Uncomment to allow write operations
-const fs = require('fs')
-const path = require('path')
-const filePath = path.join('db.json')
-const data = fs.readFileSync(filePath, "utf-8");
-const db = JSON.parse(data);
-const router = jsonServer.router(db)
-
-// Comment out to allow write operations
-// const router = jsonServer.router('db.json')
-
-const middlewares = jsonServer.defaults()
-
-server.use(middlewares)
-// Add this before server.use(router)
-server.use(router)  
-
-const port = process.env.PORT || 3000;
-
-server.listen(port, () => {
-    console.log('JSON Server is running')
-})
-
-// Export the Server API
-module.exports = server
+server.listen(port);
