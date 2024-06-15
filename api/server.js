b065062e862230cdf4947b9499c98af98391b13f
Parth <parthsata1709@gmail.com>
2024-06-15 08:58:31 +0530

    test

--- api/server.js
+++ api/server.js
@@ -1,10 +1,10 @@
-const jsonServer = require("json-server"); // importing json-server library
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
 server.use(router);
-
-server.listen(port);
+server.listen(3000, () => {
+  console.log('JSON Server is running');
+});
