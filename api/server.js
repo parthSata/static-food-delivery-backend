58c79670ad721b0eb76ca82ba1841676d5781b13
Parth <parthsata1709@gmail.com>
2024-06-15 09:03:27 +0530

    test

--- api/server.js
+++ api/server.js
@@ -2,9 +2,25 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+const fs = require('fs');
+const path = require('path');
 
 server.use(middlewares);
-server.use(router);
-server.listen(3000, () => {
-  console.log('JSON Server is running');
+
+server.use((req, res, next) => {
+  if (req.method === 'POST' || req.method === 'PUT' || req.method === 'DELETE') {
+    const filePath = path.join(__dirname, 'db.json');
+    const data = fs.readFileSync(filePath, 'utf-8');
+    req.body = JSON.parse(data);
+  }
+  next();
 });
+
+server.use(router);
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
+
+module.exports = server;
